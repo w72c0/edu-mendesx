36bfad14d8e192fe6ded187089b9aceeb62f14f3
w72c0 <a7151912@sina.com>
2021-04-27 09:27:02 +0800

    Testando os Componentes - React Testing Library

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,8 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    'cypress/global': true,
+    'cypress/globals': true,
+    'jest/globals': true,
   },
   extends: [
     'plugin:cypress/recommended',
@@ -21,7 +22,19 @@ module.exports = {
   ],
   rules: {
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
-    'import/prefer-default-export': 0,
-    'react/jsx-props-no-spreading': 'off',
+    'import/prefer-default-export': 'off',
   },
+  overrides: [
+    {
+      files: [
+        '**/*.test.js',
+      ],
+      plugins: ['jest'],
+      env: {
+        jest: true,
+      },
+      // eslint-disable-next-line global-require, import/no-extraneous-dependencies
+      ...require('eslint-plugin-jest').configs.recommended,
+    },
+  ],
 };
